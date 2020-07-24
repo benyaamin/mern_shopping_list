5eab153b1143e7d9c4c5813dbc316a154c207294
Benja Jorquera <62187223+benyaamin@users.noreply.github.com>
2020-07-24 11:34:53 -0400

    react & redux auth state

--- client/src/store.js
+++ client/src/store.js
@@ -1,19 +1,18 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 const enhancer = composeEnhancers(applyMiddleware(...middleware));
 
-const store = createStore(
-    rootReducer,
-    initialState,
-    enhancer
-);
+const store = createStore(rootReducer, initialState, enhancer);
 
 export default store;
